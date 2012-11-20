c2c465a4c103b43450e124bee7d400e5329fe45c
Huyen Tran <hey.huyen@gmail.com>
2012-11-20 16:21:33 -0500

    Rewrote client/server logic using state machine logic. Attempted using delimiters in messages. Possible blocking issues.

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import string
+import pdb
 
 HOST = sys.argv.pop() if len(sys.argv) == 3 else "127.0.0.1" 
 PORT = 8888
@@ -14,34 +15,63 @@ def make_socket():
     #print "Client: Socket created."
     return c
 
+WAITING_FOR_BOARD = 0
+WAITING_FOR_GAME_STATUS = 1
+WAITING_FOR_USER_INPUT = 2
+WAITING_TO_PLAY_AGAIN = 3
+WAITING_FOR_WELCOME = 4
+GAME_OVER = 5
+
+EOM = "::"
+
+def read_msg(data):
+    if len(data) == 0:
+        return (None, data)
+
+    EOM_index = data.find(EOM)
+    msg = data[0:EOM_index]
+    data = data[EOM_index + len(EOM):]
+    return (msg, data)
+
 if __name__ == "__main__":
     sock = make_socket()
-    sock.connect((HOST, PORT))  
-    msg = sock.recv(18) # WELCOME TO HANGMAN
-    print msg
-    print sock.recv(1024)   # HANGMAN DRAWING
-    keepGoing = True
-    while keepGoing:
-        guess = ""
-        while True:
+    sock.connect((HOST, PORT))
+
+    current_state = WAITING_FOR_WELCOME
+    data = ""
+    while current_state is not GAME_OVER:
+        if current_state is WAITING_FOR_WELCOME:
+            data += sock.recv(1024)
+            welcome, data = read_msg(data)
+            print welcome
+            current_state = WAITING_FOR_BOARD
+        elif current_state is WAITING_FOR_BOARD:
+            data += sock.recv(1024)
+            board, data = read_msg(data)
+            print board
+            current_state = WAITING_FOR_GAME_STATUS
+        elif current_state is WAITING_FOR_GAME_STATUS:
+            if not data:
+                data += sock.recv(1024) # blocking issues??
+            gameover, data = read_msg(data)
+            if gameover == "True":
+                current_state = WAITING_TO_PLAY_AGAIN
+            elif gameover == "False":
+                current_state = WAITING_FOR_USER_INPUT
+        elif current_state is WAITING_FOR_USER_INPUT:
             guess = raw_input("Guess a letter: ").lower()
             if len(guess) > 1 or guess not in string.lowercase:
                 print "Please guess a letter."
             else:
-                break
-        try:
-            sock.sendall(guess)
-        except socket.error:
-            print "Client: Send failed."
-            sys.exit()
-
-        reply = sock.recv(1024)
-        print reply
-        if "You lose." in reply:
+                sock.sendall(guess + EOM)
+                current_state = WAITING_FOR_BOARD
+        elif current_state is WAITING_TO_PLAY_AGAIN:
             play_again = raw_input("Play again? (y/n): ").lower()
-            if 'n' in play_again:
-                keepGoing = False
-                print "Thanks for playing..."
+            if "y" in play_again:
+                current_state = WAITING_FOR_WELCOME
+                sock.sendall("play" + EOM)
             else:
-                sock.sendall("1")
+                current_state = GAME_OVER
+                sock.sendall("quit" + EOM)
+                print "Thanks for playing"
     sock.close()
--- server.py
+++ server.py
@@ -16,30 +16,54 @@ def make_socket():
     s.listen(1)
     return s
 
+EOM = "::"
+
+def read_msg(data):
+    if len(data) == 0:
+        return (None, data)
+
+    EOM_index = data.find(EOM) 
+    msg = data[0:EOM_index]
+    data = data[EOM_index + len(EOM):]
+    return (msg, data)
+
 def client_thread(conn):
-    keepGoing = True
-    game = Game()
-    conn.sendall("WELCOME TO HANGMAN")
-    while keepGoing:
-        conn.sendall(game.game_string())
-        while not game.gameover:
-            data = conn.recv(1)
-            if not data:
-                break
-            reply = ""
-            if game.already_guessed(data):
-                reply += "You've already guessed %r." % data
-            else:
-                game.guess_letter(data)
-                reply += game.game_string()
-            conn.sendall(reply)
-        data = conn.recv(1)
-        if data == "1":
+    current_state = WAITING_TO_START_GAME
+    data = ""
+    while current_state is not GAME_OVER:
+        if current_state is WAITING_TO_START_GAME:
             game = Game()
-        else:
-            break
+            conn.sendall("WELCOME TO HANGMAN" + EOM)
+            conn.sendall(game.game_string() + EOM)
+            conn.sendall(str(game.gameover) + EOM)
+            current_state = WAITING_FOR_MOVE
+        elif current_state is WAITING_FOR_MOVE:
+            data += conn.recv(1024)
+            letter, data = read_msg(data)
+            if letter:
+                if game.already_guessed(letter):
+                    reply = "You've already guessed %r." % letter
+                else:
+                    game.guess_letter(letter)
+                    reply = game.game_string()
+                    if game.gameover:
+                        current_state = WAITING_TO_PLAY_AGAIN
+                conn.sendall(reply + EOM)
+                conn.sendall(str(game.gameover) + EOM)
+        elif current_state is WAITING_TO_PLAY_AGAIN:
+            data += conn.recv(1024)
+            play_again, data = read_msg(data)
+            if play_again == "play":
+                current_state = WAITING_TO_START_GAME
+            elif play_again == "quit":
+                current_state = GAME_OVER
     conn.close()
-    #print "client closed."
+
+WAITING_FOR_CONN = 1
+WAITING_FOR_MOVE = 2
+WAITING_TO_PLAY_AGAIN = 3
+WAITING_TO_START_GAME = 4
+GAME_OVER = 5
 
 if __name__ == "__main__":
     sock = make_socket()
